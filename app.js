907167138adc3c796db923539c55ddb6afe22607
hat-ad <dasashis673@gmail.com>
2021-08-05 09:47:32 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const musicList = require("./musicList");
+const PORT = process.env.PORT || 8000;
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.append("Access-Control-Allow-Methods", "GET,PATCH,POST,DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept"
+  );
+  next();
+});
+
+app.get("/get-list", (req, res) => {
+  res.status(200).json({
+    status: 200,
+    data: musicList,
+  });
+});
+
+app.get("/get-music/:id", (req, res) => {
+  const { id } = req.params;
+  const music = musicList.filter((item) => item.id === id) || [];
+  res.status(200).json({
+    status: 200,
+    data: !music.length ? "No song found!" : music[0],
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`connection is live at port : ${PORT} `);
+});
---
+++ musicList.js
@@ -0,0 +1,29 @@
+const musicList = [
+  {
+    id: "a12vd74ds3sd6584vs58sd4v",
+    name: "Gazab_Ka_Hai_Yeh_Din_-_Sanam_Re",
+    url: "https://res.cloudinary.com/ds7xxwtvb/video/upload/v1628136074/Gazab_Ka_Hai_Yeh_Din_-_Sanam_Re_Arijit_Singh_320Kbps_r8uim3.mp3",
+  },
+  {
+    id: "a12vd74ds3sd6584vs58s574",
+    name: "Barsaat_Ki_Dhun_-_Jubin_Nautiyal",
+    url: "https://res.cloudinary.com/ds7xxwtvb/video/upload/v1628136069/Barsaat_Ki_Dhun_-_Jubin_Nautiyal_dumtvs.mp3",
+  },
+  {
+    id: "a12vd74ds3sd6584kbjkbdd4v",
+    name: "Chukar_Mere_Mann_Ko",
+    url: "https://res.cloudinary.com/ds7xxwtvb/video/upload/v1628136069/03._Chukar_Mere_Mann_Ko_weeqm9.mp3",
+  },
+  {
+    id: "a12vd74ds3sd65df4vvs58sd4v",
+    name: "Teri_Meri_Kahaani_Gabbar_is_Back_Arijit_Singh",
+    url: "https://res.cloudinary.com/ds7xxwtvb/video/upload/v1628136061/Teri_Meri_Kahaani_Gabbar_is_Back_Arijit_Singh_320Kbps_jlxbuo.mp3",
+  },
+  {
+    id: "a12vd74ds3suhf524vs58sd4v",
+    name: "Saware_Phantom_Arijit_Singh",
+    url: "https://res.cloudinary.com/ds7xxwtvb/video/upload/v1628136060/Saware_Phantom_Arijit_Singh_320Kbps_f2acpl.mp3",
+  },
+];
+
+module.exports = musicList;
