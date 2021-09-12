41571d5708874cbfe244942f15a72d628f605a9c
hat-ad <dasashis673@gmail.com>
2021-09-12 18:04:45 +0530

    music list updated

--- musicList.js
+++ musicList.js
@@ -1,28 +1,48 @@
 const musicList = [
   {
     id: "a12vd74ds3sd6584vs58sd4v",
-    name: "Gazab_Ka_Hai_Yeh_Din_-_Sanam_Re",
+    name: "Gazab Ka Hai Yeh Din",
     url: "https://res.cloudinary.com/ds7xxwtvb/video/upload/v1628136074/Gazab_Ka_Hai_Yeh_Din_-_Sanam_Re_Arijit_Singh_320Kbps_r8uim3.mp3",
   },
   {
     id: "a12vd74ds3sd6584vs58s574",
-    name: "Barsaat_Ki_Dhun_-_Jubin_Nautiyal",
+    name: "Barsaat Ki Dhun",
     url: "https://res.cloudinary.com/ds7xxwtvb/video/upload/v1628136069/Barsaat_Ki_Dhun_-_Jubin_Nautiyal_dumtvs.mp3",
   },
   {
     id: "a12vd74ds3sd6584kbjkbdd4v",
-    name: "Chukar_Mere_Mann_Ko",
+    name: "Chukar Mere Mann Ko",
     url: "https://res.cloudinary.com/ds7xxwtvb/video/upload/v1628136069/03._Chukar_Mere_Mann_Ko_weeqm9.mp3",
   },
   {
     id: "a12vd74ds3sd65df4vvs58sd4v",
-    name: "Teri_Meri_Kahaani_Gabbar_is_Back_Arijit_Singh",
+    name: "Teri Meri Kahaani Gabbar is Back",
     url: "https://res.cloudinary.com/ds7xxwtvb/video/upload/v1628136061/Teri_Meri_Kahaani_Gabbar_is_Back_Arijit_Singh_320Kbps_jlxbuo.mp3",
   },
   {
     id: "a12vd74ds3suhf524vs58sd4v",
-    name: "Saware_Phantom_Arijit_Singh",
+    name: "Saware Phantom",
     url: "https://res.cloudinary.com/ds7xxwtvb/video/upload/v1628136060/Saware_Phantom_Arijit_Singh_320Kbps_f2acpl.mp3",
+  },
+  {
+    id: "a12vd74ds3suhf5kmkmrkmrpmr",
+    name: "Baarish Ban Jaana",
+    url: "https://res.cloudinary.com/ds7xxwtvb/video/upload/v1631449309/Baarish_Ban_Jaana_-_Payal_Dev_320_Kbps_jjlsqk.mp3",
+  },
+  {
+    id: "5v5v8884d8vd4f8vdf8vdgfrsd4v",
+    name: "Bedardi Se Pyaar Ka",
+    url: "https://res.cloudinary.com/ds7xxwtvb/video/upload/v1631449321/Bedardi_Se_Pyaar_Ka_-_Jubin_Nautiyal_320_Kbps_lanciy.mp3",
+  },
+  {
+    id: "a14151616161f524vs58sd4v",
+    name: "Ek Tarfa Pyar Hai Mera Pyar",
+    url: "https://res.cloudinary.com/ds7xxwtvb/video/upload/v1631449326/Ek-Tarfa-Pyar-Hai-Mera-Pyar_dizp1f.mp3",
+  },
+  {
+    id: "a12vdfyugkubkjnlns58sd4v",
+    name: "Kisi Se Pyar Ho Jaye",
+    url: "https://res.cloudinary.com/ds7xxwtvb/video/upload/v1631449334/Kisi_Se_Pyar_Ho_Jaye_-_Kaabil_320_Kbps_djom6r.mp3",
   },
 ];
 
